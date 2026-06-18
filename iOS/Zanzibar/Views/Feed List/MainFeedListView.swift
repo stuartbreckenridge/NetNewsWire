940db744915e5592cba71303eae7e16b66278113
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2026-06-18 20:23:56 +0800

    Zanzibar Folder — for all new iOS stuff, it has a z for sorting

--- iOS/Zanzibar/Views/Feed List/MainFeedListView.swift
+++ iOS/Zanzibar/Views/Feed List/MainFeedListView.swift
@@ -9,7 +9,27 @@
 import SwiftUI
 
 struct MainFeedListView: View {
-    var body: some View {
+    
+	// MARK: Environment
+	
+	// MARK: App Storage
+	
+	// MARK: State Objects
+	
+	// MARK: State
+	@State private var showAddFeedView: Bool = false
+	@State private var showAddFolderView: Bool = false
+	@State private var showCurrentActivityView: Bool = false
+	
+	// MARK: Bindings
+	
+	// MARK: Constants
+	
+	// MARK: Variables
+
+	
+	
+	var body: some View {
 		List {
 			//
 		}
@@ -30,19 +50,19 @@ struct MainFeedListView: View {
 					Image(systemName: "gear")
 				}
 				Button {
-					//
+					showCurrentActivityView = true
 				} label: {
 					Image(systemName: "arrowshape.down.circle")
 				}
 				Spacer()
 				Menu {
 					Button {
-						//
+						showAddFolderView = true
 					} label: {
 						Text(verbatim: "Add Folder")
 					}
 					Button {
-						//
+						showAddFeedView = true
 					} label: {
 						Text(verbatim: "Add Feed")
 					}
@@ -50,6 +70,26 @@ struct MainFeedListView: View {
 					Image(systemName: "plus")
 				}
 			}
+		}
+		.sheet(isPresented: $showAddFeedView) {
+			ContentUnavailableView {
+				Text(verbatim: "Add Feed View")
+			}
+		}
+		.sheet(isPresented: $showAddFolderView) {
+			ContentUnavailableView {
+				Text(verbatim: "Add Folder View")
+			}
+		}
+		.sheet(isPresented: $showCurrentActivityView) {
+			NavigationStack {
+				CurrentActivityView()
+					.presentationDetents([.medium])
+					.presentationDragIndicator(.visible)
+					.navigationTitle(Text(verbatim: "Current Activity"))
+					.navigationBarTitleDisplayMode(.inline)
+			}
+			
 		}
     }
 }
